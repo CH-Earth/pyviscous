925b774b05a8bff0a20ee1b3a3bbf70f1d50b9bb
Hongli Liu <hongliliu@Honglis-MacBook-Pro.local>
2022-09-26 19:43:33 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -9,13 +9,8 @@ except:
     
 from setuptools import find_packages
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
-requirements = ['Click>=7.0', ]
+requirements = ['xarray', 'numpy', 'pandas', 'scipy',
+                'matplotlib', 'sklearn','jupyter']
 
 test_requirements = [ ]
 
@@ -33,16 +28,14 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
-    description="Python codes you will need to implement the VISCOUS global sensitivity analysis framework",
+    description="Python codes to implement the VISCOUS global sensitivity analysis framework",
     entry_points={
         'console_scripts': [
             'pyviscous=pyviscous.cli:main',
         ],
     },
-    install_requires=['xarray', 'numpy', 'pandas', 'scipy',
-                      'matplotlib', 'sklearn','jupyter'],
+    install_requires=requirements,
     license="GNU General Public License v3",
-    long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='pyviscous',
     name='pyviscous',
@@ -50,7 +43,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/h294liu/pyviscous',
-    version='0.1.0',
+    version='0.0.1',
     zip_safe=False,
 )
 
