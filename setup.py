97f12f9a61fd52383b0c8a4b0deb1be10d8dd229
Hongli Liu <hongliliu@Honglis-MacBook-Pro.local>
2022-09-27 22:51:27 -0600

    update setup.py version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/h294liu/pyviscous',
-    version='1.0.0',
+    version='1.1.0',
     zip_safe=False,
 )
 
